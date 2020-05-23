dd5568a4fd443c1c57679f0698445c485ad1f15f
Vladislav <itochanenko@gmail.com>
2020-05-23 16:31:00 +0300

    Add wrapper class for Haval algorithm

--- Lab 3/src/main/java/com/tochanenko/common/Main.java
+++ Lab 3/src/main/java/com/tochanenko/common/Main.java
@@ -1,30 +1,20 @@
 package com.tochanenko.common;
 
-import com.tochanenko.hash.BaseHash;
 import com.tochanenko.hash.Haval;
-import com.tochanenko.hash.Util;
-
-import java.util.Base64;
+import static com.tochanenko.hash.HavalSpec.*;
 
 public class Main {
     public static void main(String[] args) {
-        Haval haval = new Haval(Haval.HAVAL_128_BIT, Haval.HAVAL_3_ROUND);
-        String string = "Hollo";
-        haval.update(string.getBytes(), 0, string.length());
-        System.out.println(Util.toString(haval.digest()));
-        haval.resetContext();
-        string = "Hello";
-        haval.update(string.getBytes(), 0, string.length());
-        System.out.println(Util.toString(haval.digest()));
+        Haval haval = new Haval(HAVAL_256_BIT, HAVAL_5_ROUND);
+        String[] strings = {
+                "Hello",
+                "Henlo",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                        "Integer et eros ultricies enim convallis mattis."
+        };
 
-        haval.resetContext();
-        string = "Mello";
-        haval.update(string.getBytes(), 0, string.length());
-        System.out.println(Util.toString(haval.digest()));
-
-        haval.resetContext();
-        string = "Nello";
-        haval.update(string.getBytes(), 0, string.length());
-        System.out.println(Util.toString(haval.digest()));
+        for (String string : strings) {
+            System.out.println(string + " : " + haval.hash(string));
+        }
     }
 }
