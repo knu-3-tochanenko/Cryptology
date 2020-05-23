5ed3fc3111b36b5a1b5ed7f3aaa79c990528dd04
Vladislav <itochanenko@gmail.com>
2020-05-23 15:36:54 +0300

    Add working Haval Hashing algorithm

--- Lab 3/src/main/java/com/tochanenko/common/Main.java
+++ Lab 3/src/main/java/com/tochanenko/common/Main.java
@@ -1,7 +1,30 @@
 package com.tochanenko.common;
 
+import com.tochanenko.hash.BaseHash;
+import com.tochanenko.hash.Haval;
+import com.tochanenko.hash.Util;
+
+import java.util.Base64;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+        Haval haval = new Haval(Haval.HAVAL_128_BIT, Haval.HAVAL_3_ROUND);
+        String string = "Hollo";
+        haval.update(string.getBytes(), 0, string.length());
+        System.out.println(Util.toString(haval.digest()));
+        haval.resetContext();
+        string = "Hello";
+        haval.update(string.getBytes(), 0, string.length());
+        System.out.println(Util.toString(haval.digest()));
+
+        haval.resetContext();
+        string = "Mello";
+        haval.update(string.getBytes(), 0, string.length());
+        System.out.println(Util.toString(haval.digest()));
+
+        haval.resetContext();
+        string = "Nello";
+        haval.update(string.getBytes(), 0, string.length());
+        System.out.println(Util.toString(haval.digest()));
     }
 }
