22adda44796c546d5ab713d4afe4f8fe017bed55
Vladislav <itochanenko@gmail.com>
2020-05-26 13:04:15 +0300

    Add example methods to Main class. Complete Lab 1

--- Lab 1/src/main/java/com/tochanenko/common/Main.java
+++ Lab 1/src/main/java/com/tochanenko/common/Main.java
@@ -1,8 +1,6 @@
 package com.tochanenko.common;
 
-import com.tochanenko.algo.ExtendedEuclidean;
-import com.tochanenko.algo.Fermat;
-import com.tochanenko.algo.MillerRabin;
+import com.tochanenko.algo.*;
 
 import java.math.BigInteger;
 import java.util.Arrays;
@@ -13,18 +11,46 @@ import java.util.Arrays;
 [x]	Реализовать тест Миллера - Рабина
 [x]	Реализовать операцию возведения по модулю в степень методом двоичного потенцирования
 [x]	Реализовать умножение Карацубы.
-[ ]	Арифметика Монтгомери
-[ ]	Расширенный алгоритм Евклида
+[x]	Арифметика Монтгомери
+[x]	Расширенный алгоритм Евклида
 
 Считать, что все числа в алгоритмах имеют длину не менее 512 бит.
 */
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ANSI.Purple("It works!"));
-        System.out.println(new Fermat(10).test(BigInteger.valueOf(7)));
-        System.out.println(new MillerRabin(10).test(BigInteger.valueOf(7)));
 
-        System.out.println(Arrays.toString(ExtendedEuclidean.compute(BigInteger.valueOf(17), BigInteger.valueOf(35))));
+        // Fermat prime number test
+        System.out.println(
+                new Fermat(10).test(BigInteger.valueOf(7))
+        );
+
+        // Miller-Rabin prime number test
+        System.out.println(
+                new MillerRabin(10).test(BigInteger.valueOf(7))
+        );
+
+        // Binary power
+        System.out.println(
+                BinaryPower.pow(BigInteger.TWO, BigInteger.valueOf(5), BigInteger.valueOf(10000))
+        );
+
+        // Multiply two numbers using Karatsuba algorithm
+        System.out.println(
+                Karatsuba.multiply(BigInteger.TWO, BigInteger.valueOf(5))
+        );
+
+        // Montgomery algorithm to multiply and power by mod
+        System.out.println(
+                new Montgomery(BigInteger.valueOf(10000)).multiply(BigInteger.TWO, BigInteger.valueOf(5))
+        );
+        System.out.println(
+                new Montgomery(BigInteger.valueOf(10000)).pow(BigInteger.TWO, BigInteger.valueOf(5))
+        );
+
+        // Extended Euclidean to find GCD and numbers to represent sum
+        System.out.println(Arrays.toString(
+                ExtendedEuclidean.compute(BigInteger.valueOf(17), BigInteger.valueOf(35))
+        ));
     }
 }
